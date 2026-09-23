103558b25d26517af854a376a9c22760627a7573
Yozora <yozoraxcii@gmail.com>
2026-09-23 08:17:17 +0100

    Push

--- tmdbapis/tmdb.py
+++ tmdbapis/tmdb.py
@@ -256,16 +256,18 @@ class TMDbAPIs:
             cast = []
             for role in value["roles"]:
                 new_dict = value.copy()
-                for k, v in role.items():
-                    new_dict[k] = v
+                if isinstance(role, dict):
+                    for k, v in role.items():
+                        new_dict[k] = v
                 cast.append(Credit(self, new_dict, credit_type="cast", media_type="tv"))
             return cast
         elif value_type == "agg_tv_crew":
             crew = []
             for role in value["jobs"]:
                 new_dict = value.copy()
-                for k, v in role.items():
-                    new_dict[k] = v
+                if isinstance(role, dict):
+                    for k, v in role.items():
+                        new_dict[k] = v
                 crew.append(Credit(self, new_dict, credit_type="crew", media_type="tv"))
             return crew
         elif value_type == "keyword":
@@ -726,11 +728,11 @@ class TMDbAPIs:
 
                * - ``created_at.asc``
                  - ``created_at.desc``
-               * - ``release_date.asc`` 
-                 - ``release_date.desc`` 
-               * - ``title.asc`` 
-                 - ``title.desc`` 
-               * - ``vote_average.asc`` 
+               * - ``release_date.asc``
+                 - ``release_date.desc``
+               * - ``title.asc``
+                 - ``title.desc``
+               * - ``vote_average.asc``
                  - ``vote_average.desc``
 
             Returns:
